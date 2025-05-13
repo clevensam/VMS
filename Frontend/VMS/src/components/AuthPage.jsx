c19903ae4780e0d3b54729c9223ee77d64c41c22
clevensam <clevensamwel@gmail.com>
2025-05-13 16:21:17 +0300

    add username input field

--- Frontend/VMS/src/components/AuthPage.jsx
+++ Frontend/VMS/src/components/AuthPage.jsx
@@ -144,6 +144,14 @@ const AuthPage = () => {
           {activeTab === 'signup' && (
             <form className="space-y-4" onSubmit={handleSubmit}>
               <div>
+<label htmlFor="username"className="block text-sm font-medium text-gray-700 mb-1 " >Username:</label>
+                 <input
+                  type="text"
+                   id="username" 
+                   placeholder="Mr.Doe"
+                   name="username"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    required></input>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">Email</label>
                 <input
                   type="email"
